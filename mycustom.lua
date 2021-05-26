4bcbd7901f7ed572003c42c3d9952c7f277c690e
RustemB <rustemb@systemli.org>
2021-05-27 04:11:11 +0500

    changes

--- mycustom.lua
+++ mycustom.lua
@@ -4,85 +4,46 @@ xplr.config.node_types.file.meta.icon = ""
 xplr.config.node_types.symlink.meta.icon = "🔗"
 xplr.config.node_types.mime_essence = {
 	text = {
-		plain = {
-			meta = { icon = "📄" },
-		},
-		markdown = {
-			meta = { icon = "" },
-		}
+		plain = { meta = { icon = "📄" }, },
+		markdown = { meta = { icon = "" }, }
 	},
 	video = {
-		["*"] = {
-			meta = { icon = "" },
-		},
+		["*"] = { meta = { icon = "" }, },
 	},
 	image = {
-		["*"] = {
-			meta = { icon = "" },
-		},
+		["*"] = { meta = { icon = "" }, },
+	},
+	application = {
+		pdf = { meta = { icon = "" }, },
+		zip = { meta = { icon = "" }, },
 	},
 }
 xplr.config.node_types.extension = {
-	py = {
-		meta = { icon = "" },
-	},
-	hy = {
-		meta = { icon = "🐙" },
-	},
-	hs = {
-		meta = { icon = "" },
-	},
-	rs = {
-		meta = { icon = "" },
-	},
-	lua = {
-		meta = { icon = "" },
-	},
-	lock = {
-		meta = { icon = "" },
-	},
-	zig = {
-		meta = { icon = "⚡" },
-	},
-	cpp = {
-		meta = { icon = "" },
-	},
-	c = {
-		meta = { icon = "" },
-	},
-	vim = {
-		meta = { icon = "" },
-	},
-	exe = {
-		meta = { icon = "" },
-	},
-	java = {
-		meta = { icon = "" },
-	},
-	js = {
-		meta = { icon = "" },
-	},
-	ts = {
-		meta = { icon = "" },
-	},
-	html = {
-		meta = { icon = "" },
-	},
-	apk = {
-		meta = { icon = "" },
-	},
-	rb = {
-		meta = { icon = "" },
-	},
+	py = { meta = { icon = "" }, },
+	hy = { meta = { icon = "🐙" }, },
+	hs = { meta = { icon = "" }, },
+	rs = { meta = { icon = "" }, },
+	lua = { meta = { icon = "" }, },
+	lock = { meta = { icon = "" }, },
+	zig = { meta = { icon = "⚡" }, },
+	cpp = { meta = { icon = "" }, },
+	c = { meta = { icon = "" }, },
+	vim = { meta = { icon = "" }, },
+	exe = { meta = { icon = "" }, },
+	java = { meta = { icon = "" }, },
+	js = { meta = { icon = "" }, },
+	ts = { meta = { icon = "" }, },
+	html = { meta = { icon = "" }, },
+	apk = { meta = { icon = "" }, },
+	rb = { meta = { icon = "" }, },
+	elm = { meta = { icon = "" }, },
+	conf = { meta = { icon = "" }, },
+	ini = { meta = { icon = "" }, },
+	toml = { meta = { icon = "" }, },
 }
 xplr.config.node_types.special = {
-	["Cargo.toml"] = {
-		meta = { icon = "" },
-	},
-	PKGBUILD = {
-		meta = { icon = "" },
-	},
-	Downloads = {
-		meta = { icon = "" },
-	},
+	["Cargo.toml"] = { meta = { icon = "" }, },
+	PKGBUILD = { meta = { icon = "" }, },
+	[".config"] = { meta = { icon = "" }, },
+	Downloads = { meta = { icon = "" }, },
 }
